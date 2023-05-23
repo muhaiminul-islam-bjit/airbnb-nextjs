f111f002d590221a098a79f7c16f9320ae37e965
BJIT_Mac <bjit_mac@Muhaimin.local>
2023-05-23 16:15:29 +0600

    feature 	- completed image upload feature

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import CountrySelect from "../inputs/CountrySelect";
 import Map from "../Map";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 enum STEPS {
   CATEGORY = 0,
@@ -51,6 +52,7 @@ const RentModal = () => {
   const roomCount = watch("roomCount");
   const bathroomCount = watch("bathroomCount");
   const location = watch("location");
+  const imageSrc = watch("imageSrc");
 
   const Map = useMemo(
     () =>
@@ -169,6 +171,21 @@ const RentModal = () => {
         />
       </div>
     );
+  }
+
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Add a photo of your place"
+          subtitle="Show guests what your place looks like!"
+        />
+        <ImageUpload
+          onChange={(value) => setCustomValue('imageSrc', value)}
+          value={imageSrc}
+        />
+      </div>
+    )
   }
 
   return (
--- next.config.js
+++ next.config.js
@@ -4,10 +4,8 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: [
-      "avatars.githubusercontent.com"
-    ]
-  }
-}
+    domains: ["avatars.githubusercontent.com", "res.cloudinary.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
