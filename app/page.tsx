f1f61e22fc3e32041ed7b1b8f97ecd3ea4556b14
BJIT_Mac <bjit_mac@Muhaimin.local>
2023-05-25 14:57:41 +0600

    feature 	- complete listing card

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,52 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import ClinetOnly from "./components/ClinetOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import getListings from "./actions/getListings";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  return <div>Hello airbnb</div>;
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClinetOnly>
+        <EmptyState showRest />
+      </ClinetOnly>
+    );
+  }
+  return (
+    <ClinetOnly>
+      <Container>
+        <div
+          className="
+      pt-24
+      grid
+      grid-cols-1
+      sm:grid-cols-2
+      md:grid-cols-3
+      lg:grid-cols-4
+      xl:grid-cols-5
+      2xl:grid-cols-6
+      gap-8
+      "
+        >
+          {listings.map((listing: any, i) => {
+            return (
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            );
+          })}
+        </div>
+      </Container>
+    </ClinetOnly>
+  );
 }
