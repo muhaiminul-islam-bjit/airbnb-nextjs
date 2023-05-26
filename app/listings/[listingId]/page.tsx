51880cba65c0fe74d88f64152453ac70d579052a
muhaiminul-islam-bjit <muhaiminul.islam@bjitgroup.com>
2023-05-26 14:51:01 +0600

    feature 	-completed individual view

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,12 +1,29 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
+import ClientOnly from "@/app/components/ClinetOnly";
+import EmptyState from "@/app/components/EmptyState";
 import React from "react";
+import ListingClient from "./ListingClient";
 
 interface IParams {
   listingId?: string;
 }
 const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingById(params);
-  return <div>{listing?.title}</div>;
+  const currentUser = await getCurrentUser();
+
+  if (!listing) {
+    return (
+      <ClientOnly>
+        <EmptyState />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <ListingClient listing={listing} currentUser={currentUser} />
+    </ClientOnly>
+  );
 };
 
 export default ListingPage;
